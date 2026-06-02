1260aabda74ff967383a4d5ccf3ca07c104c94c7
lyu2026 <lyuw2026@gmail.com>
2026-06-02 08:12:09 +0800

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,5 +38,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.compose.material:material-icons-core:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0") {
+        isTransitive = false
+    }
 }

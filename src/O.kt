59127a6d642cb0cacff7db034d5e7f5806faed11
lyu2026 <lyuw2026@gmail.com>
2026-06-02 10:55:52 +0800

    .

--- src/O.kt
+++ src/O.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment

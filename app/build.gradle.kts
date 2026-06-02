6c353cb47b530b0c1d6103f160572f600cfe707c
lyu2026 <lyuw2026@gmail.com>
2026-06-02 10:24:14 +0800

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,78 +1,77 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+	id("com.android.application")
+	id("org.jetbrains.kotlin.android")
+	id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
-    namespace = "com.fyan"
-    compileSdk = 34
+	namespace = "com.fyan"
+	compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.fyan"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+	defaultConfig {
+		applicationId = "com.fyan"
+		minSdk = 21
+		targetSdk = 34
+		versionCode = 1
+		versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-        multiDexEnabled = true
-    }
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		vectorDrawables {
+			useSupportLibrary = true
+		}
+		multiDexEnabled = true
+	}
 
-    sourceSets {
-        getByName("main") {
-            res.srcDirs("src/main/res", "${project.rootDir}/ico")
-        }
-    }
+	sourceSets {
+		getByName("main") {
+			res.srcDirs("src/main/res", "${project.rootDir}/ico")
+		}
+	}
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+	kotlinOptions {
+		jvmTarget = "17"
+	}
+	buildFeatures {
+		compose = true
+	}
+	packaging {
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.0"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+	implementation("androidx.core:core-ktx:1.12.0")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+	implementation("androidx.activity:activity-compose:1.8.2")
 
-    implementation("androidx.compose.material:material-icons-core:1.6.0")
-    implementation("androidx.compose.material3:material3")
+	implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+	implementation("androidx.compose.ui:ui")
+	implementation("androidx.compose.ui:ui-graphics")
+	implementation("androidx.compose.ui:ui-tooling-preview")
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.0"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+	implementation("androidx.compose.material:material-icons-core")
+	implementation("androidx.compose.material3:material3")
+
+	testImplementation("junit:junit:4.13.2")
+	androidTestImplementation("androidx.test.ext:junit:1.1.5")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+	androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
+	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+	debugImplementation("androidx.compose.ui:ui-tooling")
+	debugImplementation("androidx.compose.ui:ui-test-manifest")
 }

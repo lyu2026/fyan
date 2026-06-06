5964e2416fa16d015e35489129234068414a73a6
lyu2026 <lyuw2026@gmail.com>
2026-06-07 03:13:01 +0800

    .

--- .sh
+++ .sh
@@ -5,7 +5,8 @@ rm -rf app .gradle *.lock
 wget -q -P ./gradle/wrapper/ "https://services.gradle.org/distributions/gradle-8.10.2-bin.zip"
 mkdir -p app/src/main/res/{xml,mipmap-anydpi-v26,drawable}
 mkdir -p app/src/main/kotlin/com/fyan
-cp *.kt app/src/main/kotlin/com/fyan/
+mv *.kt app/src/main/kotlin/com/fyan/
+touch app/proguard-rules.pro
 
 for f in .xml .zml;do
 	[ -f "$f" ]||continue
